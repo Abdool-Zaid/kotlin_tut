f4f76d72ecdcb04fd7c24e7538342d7a5c1292b0
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-03-25 15:00:09 +0200

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,18 @@
 fun main() {
-    var inp_1 = 34
-    val inp_2 = "string of characters"
-    println("test")// prints to a new line
-    println(inp_1) // prints but does not move to new line
+    val byt_val :Byte= 127 // max = (2^7) -1 8 bits
+    val short_val : Short= 32767 // max (2^15)-1 16 bits
+    val int_val : Int =  2147483647  // max (2^31) -1 32 bits
+    val long_val : Long = 9223372036854775807 // max(2^63) -1 64 bits
 
-    val late_ex_1 = lateinit_1()
-        late_ex_1.get_arg_1()
+
+
+//    var inp_1 = 34
+//    val inp_2 = "string of characters"
+//    println("test")// prints to a new line
+//    println(inp_1) // prints but does not move to new line
+//
+//    val late_ex_1 = lateinit_1()
+//        late_ex_1.get_arg_1()
 
 }
 
