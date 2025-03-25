6a5673a5cdda1584a8870408a7c3043f8d67195b
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-03-25 14:47:04 +0200

    going to ch3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,30 @@ fun main() {
     var inp_1 = 34
     val inp_2 = "string of characters"
     println("test")// prints to a new line
-    print(inp_1) // prints but does not move to new line
+    println(inp_1) // prints but does not move to new line
+
+    val late_ex_1 = lateinit_1()
+        late_ex_1.get_arg_1()
+
+}
+
+
+class lateinit_1 {
+    lateinit var arg_1: String  //lateinit allows for valueless declarations
+
+    fun set_arg_1 (set_var: String){
+            arg_1= set_var
+    }
+
+    fun get_arg_1(){
+         if (::arg_1.isInitialized){
+             println(arg_1)
+         }else{
+             println("arg_1 is not initialized")
+         }
+
+    }
+
+
 
 }
