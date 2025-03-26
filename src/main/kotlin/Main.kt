de30a58fd03eb348ad44f6fb99cfba16816bd5b7
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-03-26 15:09:00 +0200

    ch7

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 
 
 fun main() {
-    val  arr_1 : Array<String> = arrayOf("str1", "str2", "str3")
-    val arr_2 = Array(7){1}
-    println(arr_2.contentToString()) // prints array as string
-    println(arr_1.indexOf("str2"))
-    println(arr_2.sum())
+   val matrix_1 = Array(7){
+       Array(2){4}
+   }
+
+    println(matrix_1.contentDeepToString())
 }
 
 
