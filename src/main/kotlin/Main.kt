8fb5929a7c6f7984bb18cc3d9e3966cdfcdc27d8
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-03-26 14:34:35 +0200

    ch5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,49 +1,24 @@
 
-const val  arg_2 = 56 // const only works outside of functions
 
 fun main() {
-    
+    val  string_val : String = "string goes here" // immutable
+    val  multiline_string : String ="""
+        string
+        goes 
+        here
+    """.trimMargin()
+
+    println("string val = $string_val") // string template
+    println(" length = ${string_val.length}") // need braces for expressions
+    println(multiline_string)
+    println(string_val.substring(0,4)) // runs till before arg2
 
 
-//    val byt_val :Byte= 127 // max = (2^7) -1 8 bits
-//    val short_val : Short= 32767 // max (2^15)-1 16 bits
-//    val int_val : Int =  2147483647  // max (2^31) -1 32 bits
-//    val long_val : Long = 9223372036854775807 // max(2^63) -1 64 bits
-//    val float_val : Float= 67.12f // must end with f  mantissa 6-7 32 bits
-//    val double_val : Double = 67.67867823768 // mantissa 15-16 64 bits
-//    val char_val : Char ='s' // must be single quote
-//    val bool_val : Boolean = true
 
+//string builders and string buffers are mutable
 
-    println(bool_val)
-
-//    var inp_1 = 34
-//    val inp_2 = "string of characters"
-//    println("test")// prints to a new line
-//    println(inp_1) // prints but does not move to new line
-//
-//    val late_ex_1 = lateinit_1()
-//        late_ex_1.get_arg_1()
-
+    val string_build =StringBuilder() //not thread safe
+    val string_buffer = StringBuffer("buffered")  // thread safe
 }
 
 
-class lateinit_1 {
-    lateinit var arg_1: String  //lateinit allows for valueless declarations
-
-    fun set_arg_1 (set_var: String){
-            arg_1= set_var
-    }
-
-    fun get_arg_1(){
-         if (::arg_1.isInitialized){
-             println(arg_1)
-         }else{
-             println("arg_1 is not initialized")
-         }
-
-    }
-
-
-
-}
