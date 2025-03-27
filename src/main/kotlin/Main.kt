32117489925b67dcf31d4bec38d48be932e49223
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-03-27 16:09:17 +0200

    ch9 oppperations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,21 @@
 // logic opperators && || !
 fun main() {
-var num =4
-    println(num++) //uses number then increments it
-    println(++num) // increments number then uses it
+    val arr= Array(10){Math.random()*10}
+for ((index, i) in arr.withIndex()){ // have to call withIndex() if you want the index
+    if (index % 2 == 0) continue // skips even numbers
+    println("$index = $i")
+}
+    var i=1
+ while (i <= 10){
+     println(i)
+     i++
+ }
+    var  ind= 1
+    while (true){
 
-//    assignment opps
-    num += 3
-    num -= 3
-
+        println(ind)
+       if(ind++  == 10) break
+    }
 }
 
 
