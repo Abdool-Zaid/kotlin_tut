6aaabd33d988b299e21660e0da4d639b0bd1ecfa
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-03-27 15:40:10 +0200

    ch9 opperations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,12 @@
-
-const val adult = 18
-
+// logic opperators && || !
 fun main() {
-    val age= 18
-    val is_legal = if (age>= adult)true else false
+var num =4
+    println(num++) //uses number then increments it
+    println(++num) // increments number then uses it
 
-    println(is_legal)
-    if (age >=adult){
-        println("is an adult") // last value is always returned
-    }else{
-        println("is not an adult")
-    }
-if (age>0 && age <=11){
-    println("is a child")
-}else if (age>11 && age <=17){
-    println("is a teenager")
-}else if (age>=adult){ // inequalities do not consider value unless stated with =
-    println("is an adult")
-}else{
-    println("entered invalid age")
-}
-
-    when(is_legal){
-    true-> println("is an adult") // again last line is always returned
-    false -> println("is not at adult")
-    }
+//    assignment opps
+    num += 3
+    num -= 3
 
 }
 
