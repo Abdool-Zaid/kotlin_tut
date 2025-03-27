19785b3956cc8ca17e0f75c0417ccf9e4bac6384
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-03-27 16:34:05 +0200

    ch12 ranges

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,17 @@
-// logic opperators && || !
 fun main() {
-println("enter value")
-    val arg_1 = readln()
-    println(arg_1)
+    val range_val =1 .. 17
+    for (i in range_val){
+        println(i)
+    }
+    for (i in 1..20){
+        if (i%2!=0)continue 
+        println(i)
+    }
+    for (i in 1..20 step 2) println(i)
+
+    for (i in 1000 downTo 1  step 7) println(i) //for the weebs
+    for( i in 'z' downTo 'a') println(i)
+
 }
 
 
