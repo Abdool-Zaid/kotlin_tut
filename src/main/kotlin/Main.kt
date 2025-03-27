de367d0a0fb8ba9125e80ca41bb77fa5e6d05f83
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-03-27 16:14:23 +0200

    ch11 console inputs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,8 @@
 // logic opperators && || !
 fun main() {
-    val arr= Array(10){Math.random()*10}
-for ((index, i) in arr.withIndex()){ // have to call withIndex() if you want the index
-    if (index % 2 == 0) continue // skips even numbers
-    println("$index = $i")
-}
-    var i=1
- while (i <= 10){
-     println(i)
-     i++
- }
-    var  ind= 1
-    while (true){
-
-        println(ind)
-       if(ind++  == 10) break
-    }
+println("enter value")
+    val arg_1 = readln()
+    println(arg_1)
 }
 
 
