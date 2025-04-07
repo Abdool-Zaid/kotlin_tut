2a806db3f765e9da3f4513539a525992cf294712
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-04-07 13:59:32 +0200

    ch20_interfaces

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,38 +1,24 @@
 fun main(){
-    val object_1 = obj(name = "object_1", location = "there")
-    val object_2 = shape(vertices = 5, colour = "gold", name= "shape_1")
-    println(object_2.get_name())
+val Obj_1 = Object( "Object_1")
+    Obj_1.abstract_method()
 }
 
-open class obj(
-    private val name: String = "default object",
-    private var location:String= "here"
-) {
-    init {//  this method is called when new instance is created
-        println("new object: $name was created at location: $location")
+interface Interface_1{
 
-    }
-    fun get_name(): String {
-        return this.name
-    }
-    fun get_locations(): String{
-        return  this.location
-    }
-
-    fun set_location(place: String){
-        this.location= place
+    fun abstract_method()
+    fun test_cal(){
+        println("test method from Interface_1")
     }
 
 
 }
 
-class shape(
-    private val vertices: Int= 3,
-    private var colour : String= "green",
-    private val name:String
-):obj(name = name, location = "default shape location"){
-init {
-    println("additionally $name is a shape")
-}
+class Object(
+    private val Name : String
+) : Interface_1{
+
+    override fun abstract_method() {
+        println(this.Name)
+    }
 
 }
