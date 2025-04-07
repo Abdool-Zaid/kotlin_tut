a9a825608eee127c88a020d2b69a3e63e47bd71a
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-04-07 11:20:34 +0200

    exceptions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,16 @@
-fun main() {
-    val range_val =1 .. 17
-    for (i in range_val){
-        println(i)
-    }
-    for (i in 1..20){
-        if (i%2!=0)continue 
-        println(i)
-    }
-    for (i in 1..20 step 2) println(i)
+import kotlin.reflect.typeOf
 
-    for (i in 1000 downTo 1  step 7) println(i) //for the weebs
-    for( i in 'z' downTo 'a') println(i)
+fun main(){
+
+    try {
+    val res = "2423sdf"
+        val final = res.toInt()
+    }catch (e: NumberFormatException){
+        print(e.message)
+    }catch (e:Exception){
+    print(e)
+    }finally {
+        print("always executed, with or without error")
+    }
 
 }
-
-
-
