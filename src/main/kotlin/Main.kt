437960bd07e354f4e3ee8e761d19eba7a9aeb9d4
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-04-07 13:20:42 +0200

    classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
 fun main(){
-println("test_string_1".call())
+    val object_1 = obj(name = "object_1", location = "there")
+    val  object_2 = obj(object_1)
 }
-fun String.call():String{
 
-    return "called by String: ${this}"
+class obj(
+    private val name: String = "default object",
+    private var location:String= "here"
+) {
+    init {//  this method is called when new instance is created
+        println("new object: $name was created at location: $location")
+
+    }
+    fun get_name(): String {
+        return this.name
+    }
+    fun get_locations(): String{
+        return  this.location
+    }
+
+    fun set_location(place: String){
+        this.location= place
+    }
+
+
 }
