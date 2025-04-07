16366c295fa0bd0863cc0f5b0e1ebd6d9a96e650
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-04-07 20:32:19 +0200

    ch20

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
+import java.lang.IllegalArgumentException
+import javax.swing.text.Position
+
 fun main(){
 val Obj_1 = Object( "Object_1")
+    Obj_1.Position= "s"
     Obj_1.abstract_method()
+
 }
 
 interface Interface_1{
@@ -14,11 +19,23 @@ interface Interface_1{
 }
 
 class Object(
-    private val Name : String
+    private val Name : String,
+   var _Position : String ="here"
 ) : Interface_1{
 
+
+ var Position:String = _Position
+        get() = field.uppercase()
+        set(value) {
+            if(value.length <=3){
+               throw IllegalArgumentException("Position length must be greater than 3, ${value} is too short")
+            }else{
+                field= value.uppercase()
+            }
+
+        }
     override fun abstract_method() {
-        println(this.Name)
+        println("${this.Name}, ${this.Position}")
     }
 
 }
