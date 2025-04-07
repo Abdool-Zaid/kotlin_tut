989c83d1304a5774c649fbfdebd5f3eb76d48b0d
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-04-07 11:44:38 +0200

    functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,41 @@
-import kotlin.reflect.typeOf
-
 fun main(){
+//    println(division(12,9))
+    println(sum_all(1,2,3,4,5,6,7,8,9,10))
+}
 
+fun division (a: Int, b:Int):Int{ // return type is not required
+    var res: Int = 0
     try {
-    val res = "2423sdf"
-        val final = res.toInt()
-    }catch (e: NumberFormatException){
-        print(e.message)
+        res= a/b
     }catch (e:Exception){
-    print(e)
+        println(e.message)
     }finally {
-        print("always executed, with or without error")
+        return res
+    }
+}
+
+fun sum_all (vararg numbers:Int):Int{
+    var res = 0
+//    println(numbers.size)
+    if (numbers.size<= 1 ) {
+        println("too little elements")
+        return res
+    }
+    else if(numbers.size>=20000){
+        println("too many elements ")
+        return  res
+    }
+    else{
+        try {
+            for (i in numbers){
+                res= res + i
+//                println(res)
+            }
+        }catch (e: Exception){
+            println(e)
+        }finally {
+            return  res
+        }
     }
 
 }
